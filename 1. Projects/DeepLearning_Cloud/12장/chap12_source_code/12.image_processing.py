bc4a38daa07d5d2269c88e511a6d34521244b141
정다훈 <jdh251425142514@gmail.com>
2024-11-05 10:11:35 +0900

    vault backup: 2024-11-05 10:11:35

--- 1. Projects/DeepLearning_Cloud/12장/chap12_source_code/12.image_processing.py
+++ 1. Projects/DeepLearning_Cloud/12장/chap12_source_code/12.image_processing.py
@@ -5,15 +5,16 @@ from tkinter.filedialog import askopenfilename  # choose file
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg 
-# %matplotlib inline
+import numpy as np  # numpy 추가
 
 fname = askopenfilename()           # choose image file
 image = mpimg.imread(fname)         # read image
 plt.imshow(image)                   # display image 
+plt.axis('off')  # 축 제거
 plt.show()
 
 # type of image object
-type(image)
+print(type(image))
 
 # image shape
 print(image.shape)
@@ -28,37 +29,39 @@ from skimage import color
 gray_image = color.rgb2gray(image)
 print(gray_image.shape)
 plt.imshow(gray_image, cmap='gray')  
+plt.axis('off')  # 축 제거
 plt.show()
 
 # resize
 from skimage import transform 
 
-new_shape = (image.shape[0] // 2, image.shape[1] // 2, 
-             image.shape[2])
+new_shape = (image.shape[0] // 2, image.shape[1] // 2)  # 채널 수는 필요 없음
 small = transform.resize(image=image, 
         output_shape=new_shape)
 
 print(small.shape)
 plt.imshow(small)
+plt.axis('off')  # 축 제거
 plt.show()
               
 
-# filp, rotate
+# flip, rotate
 from skimage.transform import rotate
 
-plt.imshow(np.fliplr(image))  # filp
-plt.imshow(rotate(image, angle=45))  # rotate
+plt.imshow(np.fliplr(image))  # flip
+plt.axis('off')  # 축 제거
+plt.show()
+
+plt.imshow(rotate(image, angle=45, resize=True))  # rotate (resize=True 추가)
+plt.axis('off')  # 축 제거
 plt.show()
 
 
 # filters
-from skimage.filters import sobel_h
-plt.imshow(sobel_h(gray_image))
+from skimage.filters import sobel
+plt.imshow(sobel(gray_image))  # sobel_h 대신 sobel 사용
+plt.axis('off')  # 축 제거
 plt.show()
 
 # save image
 io.imsave("d:/data/test2.png", image)
-
-
-
-                  
