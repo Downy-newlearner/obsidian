2317f2830977869fdd4267045253abdd5469cbd5
정다훈 <jdh251425142514@gmail.com>
2024-11-12 10:00:00 +0900

    vault backup: 2024-11-12 09:59:59

--- 1. Projects/DeepLearning_Cloud/14장_keras_transfer_learning/14_source_code/14.VGG_case1.py
+++ 1. Projects/DeepLearning_Cloud/14장_keras_transfer_learning/14_source_code/14.VGG_case1.py
@@ -6,11 +6,9 @@ from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
 
-# load the model
-model = VGG16()       # take a long time 
 
 # load an image from file
-image = load_img("C:\Obsidian\obsidian\1. Projects\DeepLearning_Cloud\14장_keras_transfer_learning\whatisit.jpeg", target_size=(224, 224))
+image = load_img("C:/Obsidian/obsidian/1. Projects/DeepLearning_Cloud/14장_keras_transfer_learning/whatisit.jpeg", target_size=(224, 224))
 
 # convert the image pixels to a numpy array
 image = img_to_array(image)
@@ -21,11 +19,16 @@ image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 # prepare the image for the VGG model
 image = preprocess_input(image)
 
+# load the model
+model = VGG16()       # take a long time 
+
 # predict the probability across all output classes
 pred = model.predict(image)
 
 # convert the probabilities to class labels
 label = decode_predictions(pred)
+
+print(label)
 
 # retrieve the most likely result, e.g. highest probability
 label = label[0][0]
