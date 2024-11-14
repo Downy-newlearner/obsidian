30fa409328aaccfc44b82c4dfd91b88e59d2ddc5
정다훈 <jdh251425142514@gmail.com>
2024-11-14 09:48:04 +0900

    vault backup: 2024-11-14 09:48:04

--- 1. Projects/DeepLearning_Cloud/강의 노트/14장_keras_transfer_learning/prac1/prac_code_dc.py
+++ 1. Projects/DeepLearning_Cloud/강의 노트/14장_keras_transfer_learning/prac1/prac_code_dc.py
@@ -3,8 +3,9 @@ import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLabel
 from PyQt5.QtGui import QPixmap
 from PIL import Image
-from tensorflow.keras.applications.efficientnet import EfficientNetB0
+from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+
 
 class ImageLoaderApp(QMainWindow):
     def __init__(self):
