292d06d18a026f5d04b373a78724c939f77ba689
Downy-newlearner <jdh251425142514@gmail.com>
2024-11-24 18:07:51 +0900

    vault backup: 2024-11-24 18:07:51

--- 1. Projects/DeepLearning_Cloud/강의 노트/15장_keras_GAN/chap15_source_code/15_Object dection example.py
+++ 1. Projects/DeepLearning_Cloud/강의 노트/15장_keras_GAN/chap15_source_code/15_Object dection example.py
@@ -37,7 +37,7 @@ plt.show()
 
 # resize image
 inference_resizing = keras_cv.layers.Resizing(
-    640, 640, pad_to_aspect_ratio=True, bounding_box_format="xywh"
+    640, 640, pad_to_aspect_ratio=False, bounding_box_format="xywh"
 )
 
 
