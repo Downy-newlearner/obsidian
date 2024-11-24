31acba5defe30050ce5add8d66479dd7b4de3b44
Downy-newlearner <jdh251425142514@gmail.com>
2024-11-24 18:37:44 +0900

    vault backup: 2024-11-24 18:37:43

--- 1. Projects/DeepLearning_Cloud/강의 노트/15장_keras_GAN/chap15_source_code/tesst.py
+++ 1. Projects/DeepLearning_Cloud/강의 노트/15장_keras_GAN/chap15_source_code/tesst.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-arr = np.array([1, 2, 3])
-print("Original", arr)  # [1 2 3]
-print("Original Shape:", arr.shape)  # (3,)
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+print("Original Shape:", arr.shape)  # (2, 3)
 
-# 차원을 추가
-arr_expanded = np.expand_dims(arr, axis=1)
-print("Expanded", arr_expanded)  # [[1 2 3]]
-print("Expanded Shape:", arr_expanded.shape)  # (1, 3)
+# 앞에 차원 추가
+arr_expanded_0 = np.expand_dims(arr, axis=0)
+print(arr_expanded_0.shape)  # (1, 2, 3)
+
+# 중간에 차원 추가
+arr_expanded_1 = np.expand_dims(arr, axis=1)
+print(arr_expanded_1.shape)  # (2, 1, 3)
+
+# 뒤에 차원 추가
+arr_expanded_2 = np.expand_dims(arr, axis=-1)
+print(arr_expanded_2.shape)  # (2, 3, 1)
