82a658bf4da4cb589267c14c23f42ecf5fe7dacb
Downy-newlearner <jdh251425142514@gmail.com>
2025-01-08 22:52:00 +0900

    vault backup: 2025-01-08 22:52:00

--- 1. Projects/2025 1월 파이썬, RAG 스터디/practice code/generator.py
+++ 1. Projects/2025 1월 파이썬, RAG 스터디/practice code/generator.py
@@ -9,9 +9,23 @@ def fibon(n):
     return result
 
 
+#start = time.time()
+#for x in fibon(100000):
+#    pass
+
+#end = time.time()
+#print('total time: ', end - start)
+
+def fibon_generator(n):
+    a = b = 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
 start = time.time()
-for x in fibon(1000000):
+for x in fibon_generator(100000):
     pass
 
 end = time.time()
+
 print('total time: ', end - start)
